c32887aa8eaeca11066e1fc9f10f9bed8b74f672
Ryan Ghaida <ryaboug@gmail.com>
2018-02-03 13:24:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ function add(a, b) {
   return sum 
 }
 
+function subtract(a,b) {
+  minusSum = a - b 
+  return minusSum
+}
+
+function multiply (a,b) {
+product = a * b
+return product 
+}
+
+function divide(a,b) {
+  quotient = a/b 
+  return quotient 
+}
