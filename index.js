e2e8e7f22eacf0154c464862bf31fce114eddeb7
Ryan Ghaida <ryaboug@gmail.com>
2018-02-03 13:50:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function dec(n) {
 }
   
 function makeInt(n, base) {
-    var parsed = parseInt('n', 10)
+    var parsed = parseInt(n, base)
       if (isNaN(parsed)) { return 0 }
   return parsed
 }
